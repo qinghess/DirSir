2c32f3901495b3fc59ca8bebddffdd71e3e0bc64
duandabao <2351845603@qq.com>
2022-08-20 11:38:55 +0800

    8.20-第一次提交

---
+++ 电商/utils/an.js
@@ -0,0 +1,49 @@
+export default [
+    {   
+        id:0,
+        name:"限时抢购",
+        name1:"0元抢新品",
+        many:29.9,
+        img:"http://blog.img.duanshuilu.com/p2sd12fsd1fsd12fs.jpg",
+        many1:4.9,
+        img1:"http://blog.img.duanshuilu.com/ds7sd7f7sd7f7dsf.jpg"
+    },
+    {   
+        id:1,
+        name:"时令新品",
+        name1:":",
+        many:35.9,
+        img:"http://blog.img.duanshuilu.com/6d6sf9wer9ew9ds66dsfdfdsf.jpg",
+        many1:5,
+        img1:"http://blog.img.duanshuilu.com/ds5sdf5d5sf5ds5f5d2fds2fds02f.jpg"
+    },
+    {   
+        id:2,
+        name:"平价菜场",
+        many:"5.7折",
+        img:"http://blog.img.duanshuilu.com/2sd2f2ds2f2f5erertewewrewr.jpg",
+        name1:"发现好货",
+        many1:9.9,
+        img1:"http://blog.img.duanshuilu.com/qq3q33d3sf3ds3f.jpg"
+    },
+    
+    {   
+        id:3,
+        name:"绿卡会员",
+        img:"http://blog.img.duanshuilu.com/6d6sf9wer9ew9ds66dsfdfdsf.jpg",
+        name1:"宝妈严选",
+        img1:"http://blog.img.duanshuilu.com/sdafdf323d3sf3ds3fdssdfdsaf.jpg",
+        many:9.9,
+        many1:9.9,
+    },
+    {   
+        id:4,
+        name:"轮播图区",
+        many:"5.7折",
+        img:"http://blog.img.duanshuilu.com/sdfew333ds3fdsfdsdsfdfsfds.jpg",
+        name1:"轮播图区",
+        many1:9.9,
+        img1:"http://blog.img.duanshuilu.com/safsgfsfsdfs3d3dsf3sd3fdsf.jpg"
+    },
+   
+]
