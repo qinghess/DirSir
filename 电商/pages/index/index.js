6cfabf4af77e52ec45ef25b9e15f7537409596fa
ZhouSer <1921336657@qq.com>
2022-08-21 10:06:17 +0800

    duandabao8.21,10:06推送

--- 电商/pages/index/index.js
+++ 电商/pages/index/index.js
@@ -1,50 +1,56 @@
 // index.js
-import pro from "../../utils/product"
-// 获取应用实例
-const app = getApp()
+import pro from "../../utils/product";
+import duanData from "../../utils/an";
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
     pro,
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    duanData,
+    pingData: [{
+      "time": "3599000",
+    }],
   },
   onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      listData: this.data.pingData
     })
-  }
+    this.setCountDown();
+  },
+
+  // 倒计时
+  setCountDown: function () {
+    let time = 1000;
+    let { listData } = this.data;
+    let list = listData.map((v, i) => {
+      if (v.time <= 0) {
+        v.time = 0;
+      }
+      let formatTime = this.getFormat(v.time);
+      v.time -= time;
+      v.countDown = `${formatTime.mm}:${formatTime.ss}`;
+      return v;
+    });
+    this.setData({
+      listData: list
+    });
+    setTimeout(this.setCountDown, time);
+  },
+  // 格式化时间
+  getFormat: function (msec) {
+    let ss = parseInt(msec / 1000);
+    let mm = 0;
+    if (ss > 60) {
+      mm = parseInt(ss / 60);
+      ss = parseInt(ss % 60);
+      if (mm > 60) {
+        mm = parseInt(mm % 60);
+      }
+    }
+    ss = ss > 9 ? ss : `0${ss}`;
+    mm = mm > 9 ? mm : `0${mm}`;
+    return {
+      ss,
+      mm,
+    };
+  },
 })
